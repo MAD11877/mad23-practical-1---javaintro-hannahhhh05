aa819f8a0a41f61034dd25de0ebdc03a47fb73b1
Lim_Jin_Xin_Hannah <s10243399@connect.np.edu.sg>
2023-07-01 01:38:04 +0800

    First Commit

--- Question3.java
+++ Question3.java
@@ -10,7 +10,23 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
+    // Create a Scanner object to read user input
     Scanner in = new Scanner(System.in);
+
+    // Prompt the user to enter an integer
+    System.out.print("Enter an integer: ");
+        
+    // Read the integer entered by the user
+    int number = in.nextInt();
+    
+    // Multiply the integer by itself
+    int result = number * number;
+    
+    // Print the result
+    System.out.printf("The result of %d multiplied by itself is %d", number, result);
+    
+    // Close the scanner object
+    in.close();
     
   }
 }
--- Question5.java
+++ Question5.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Question5
 {
@@ -26,7 +26,61 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
+     Scanner scanner = new Scanner(System.in);
+        
+     // Prompt the user to enter the number of integers
+     System.out.print("Enter the number of integers: ");
+     
+     // Read the number of integers entered by the user
+     int n = scanner.nextInt();
+     
+     // Create an array to store the integers
+     int[] arr = new int[n];
+     
+     // Prompt the user to enter the integers
+     System.out.printf("Enter %d integers:%n", n);
+     
+     // Read in the integers entered by the user
+     for (int i = 0; i < n; i++) {
+         arr[i] = scanner.nextInt();
+     }
+     
+     // Close the scanner object
+     scanner.close();
+
+     // Find the mode (highest occurrence) in the array
+     int mode = findMode(arr);
+     
+     // Print out the mode
+     System.out.printf("The mode of the set of integers is %d%n", mode);
+ }
+
+
+     public static int findMode(int[] arr) {
+         // Sort the array in ascending order
+         Arrays.sort(arr);
+         
+         int maxFreq = 0;
+         int mode = arr[0];
+         int freq = 1;
+         for (int i = 1; i < arr.length; i++) {
+             if (arr[i] == arr[i - 1]) {
+                 freq++;
+             } else {
+                 if (freq > maxFreq) {
+                     maxFreq = freq;
+                     mode = arr[i - 1];
+                 }
+                 freq = 1;
+             }
+         }
+         
+         // Check for the last element
+         if (freq > maxFreq) {
+             mode = arr[arr.length - 1];
+         }
+         
+         return mode;
     
   }
 }
